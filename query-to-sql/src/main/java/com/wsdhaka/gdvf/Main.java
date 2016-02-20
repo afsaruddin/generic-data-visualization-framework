b9995ce19652a7a684cc1193aab0f67072a55028
Afsar Uddin <afsar.uddin@widespace.com>
2016-02-21 02:40:07 +0600

    /submitquery added [Dummy]

--- query-to-sql/src/main/java/com/wsdhaka/gdvf/Main.java
+++ query-to-sql/src/main/java/com/wsdhaka/gdvf/Main.java
@@ -6,5 +6,16 @@ import spark.Spark;
 public class Main {
     public static void main(String[] args) {
         Spark.get("/querytosql", "application/json", (req, res) -> new Query2SQLHandler().doGet(req, res), new JsonTransformer());
+        Spark.get("/submitquery", "application/json", (req, res) -> getDummyDataForResult());
+    }
+
+    private static String getDummyDataForResult() {
+        return " [" +
+                "{'age': 20, 'population': 7040659}," +
+                "{'age': 25, 'population': 7704659 }," +
+                "{'age': 30, 'population': 27046599 }," +
+                "{'age': 35, 'population': 2804659 }," +
+                "{'age': 40, 'population' : 2704659 }" +
+                "]";
     }
 }
