d83175e20952ca8ed93515147265f3821f108dae
Shafiul tareq <shafiul.hasan@widespace.com>
2016-02-21 17:45:08 +0600

    Final api is fixed and now we are serving data to the caller in JSON

--- query-handler/controller/querycontroller.py
+++ query-handler/controller/querycontroller.py
@@ -1,7 +1,8 @@
 from flask import current_app
 from util.httputil import  HttpUtil
+from util.dbutil import DbUtil
 import pprint
-
+import json
 
 
 class QueryHandlerController:
@@ -15,19 +16,35 @@ class QueryHandlerController:
         #print(query)
         url = current_app.config.get('QUERY_PROCESSING_URL')
 
+        #print ("url is {}").format(url)
 
-        pprint.pprint( HttpUtil.post_url( url ))
+        record_data = HttpUtil.post_url(url, query)
 
-        print ('Quries will be proceed soon')
+        #pprint.pprint(record_data)
+        #print ('Quries will be proceed soon')
 
+        return record_data
 
-        return ret_values
 
     def execute_query(self, sql_query):
 
-        print ('Query handlers execute_query method is called')
+        query_data = DbUtil.execute_query(sql_query)
+        processed_datas = []
 
 
+        if len(query_data) > 0:
 
+            i =0
+            for x in query_data['data']:
+                j=0
+                processed_data = {}
+                for y in query_data['column_names']:
+                    processed_data[y] = x[j]
+                    j = j+1
+                processed_datas.append(processed_data)
+                i=i+1
 
-        pass
+            return {'success': True,
+             'data': processed_datas}
+        else:
+            return {'success': False}
--- query-handler/util/dbutil.py
+++ query-handler/util/dbutil.py
@@ -4,12 +4,12 @@ import pprint
 
 class DbUtil(object):
     @staticmethod
-    def execute_query(self, query):
+    def execute_query( query):
 
         conn_string = "host='localhost' dbname='gdvf' user='gdvf_user' password='gdvf_PASS'"
 
         # print the connection string we will use to connect
-        print "Connecting to database\n	->%s" % (conn_string)
+        #print "Connecting to database\n	->%s" % (conn_string)
 
         # get a connection, if a connect cannot be made an exception will be raised here
         conn = psycopg2.connect(conn_string)
@@ -21,10 +21,9 @@ class DbUtil(object):
         cursor.execute(query)
 
         # retrieve the records from the database
+
+        column_names = [desc[0] for desc in cursor.description]
         records = cursor.fetchall()
 
-
-        # Will remove this
-        pprint.pprint(records)
-
-        return records
+        return {'column_names': column_names,
+                'data': records }
