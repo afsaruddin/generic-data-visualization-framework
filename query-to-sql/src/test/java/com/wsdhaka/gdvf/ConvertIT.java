6c9022b437ab48fb05a1f78b63529b1f7eee11a9
Afsar Uddin <afsar.uddin@widespace.com>
2016-02-21 14:26:44 +0600

    Some sanity check

--- query-to-sql/src/test/java/com/wsdhaka/gdvf/ConvertIT.java
+++ query-to-sql/src/test/java/com/wsdhaka/gdvf/ConvertIT.java
@@ -27,14 +27,35 @@ public class ConvertIT extends BaseIT {
     @Test
     public void testOk() throws IOException {
         testQueryToSQL("{\"select\": [ \"location\" ]}", "SELECT name FROM location");
+        testQueryToSQL("{\"select\": [ \"location\" ], \"something\": \"else\"}", "SELECT name FROM location");
         testQueryToSQL("{\"select\": [ \"name\" ]}", "SELECT name FROM profession");
         testQueryToSQL("{\"select\": [ \"tour\", \"schedule\" ]}", "SELECT startTime, endTime, costPerPerson FROM tour");
         testQueryToSQL("{\"select\": [ \"tour\", \"cost\" ]}", "SELECT startTime, endTime, costPerPerson FROM tour");
+    }
+
+    @Test
+    public void testFailed() throws IOException {
+        testFailedFor400("");
+        testFailedFor400("abcdef");
+        testFailedFor400("{}");
+        testFailedFor400("true");
+        testFailedFor400("null");
+        testFailedFor400("false");
+        testFailedFor400("{\"something\": 10}");
     }
 
     private void testQueryToSQL(String humanQuery, String expectedSQL) throws IOException {
         JSONObject response = new JSONObject(RESTUtils.doPost(HTTP_QUERY_TO_SQL_HOST + "/querytosql", humanQuery));
         Assert.assertNotNull(response);
         Assert.assertEquals(expectedSQL, response.getString("sql"));
+    }
+
+    private void testFailedFor400(String dataToSend) {
+        try {
+            RESTUtils.doPost(HTTP_QUERY_TO_SQL_HOST + "/querytosql", dataToSend);
+            Assert.fail("REST call should not be okay.");
+        } catch (IOException e) {
+            Assert.assertTrue(e.getMessage().contains("400 Bad Request"));
+        }
     }
 }
