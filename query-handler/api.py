a6d117eb97c423a22071da2027e467799154ac2f
Shafiul tareq <shafiul.hasan@widespace.com>
2016-02-21 19:12:43 +0600

    Enabling Cross domain ajax calling

--- query-handler/api.py
+++ query-handler/api.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_restful import Api
+from flask.ext.cors import CORS
 
 from resources.queryhandler import QueryHandler
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 app.config.from_object('settings.configuemodule.DevelopmentConfig')
