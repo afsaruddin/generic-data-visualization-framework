69d9f7ec60cff2c8051a6cb411f69e97c1ef106e
Afsar Uddin <afsar.uddin@widespace.com>
2016-02-21 16:50:52 +0600

    QueryTokenizer MS is used for query tokenizing

--- querytoken-to-sql/src/main/java/com/wsdhaka/gdvf/query2sql/Query2SQLHandler.java
+++ querytoken-to-sql/src/main/java/com/wsdhaka/gdvf/query2sql/Query2SQLHandler.java
@@ -1,45 +1,60 @@
 package com.wsdhaka.gdvf.query2sql;
 
+import com.wsdhaka.gdvf.query2sql.soa.querytokenizer.QueryTokenizerResponse;
+import com.wsdhaka.gdvf.query2sql.soa.querytokenizer.QueryTokenizerSOA;
 import com.wsdhaka.gdvf.utils.JSONUtils;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import spark.Request;
 import spark.Response;
 
+import java.io.IOException;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Query2SQLHandler {
-    public Query2SQLResponse doGet(Request req, Response res) {
+
+    public static final Query2SQLResponse EMPTY_RESPONSE = new Query2SQLResponse(Collections.emptyList(), "");
+
+    public Query2SQLResponse doGet(Request req, Response res) throws IOException {
         return convert(JSONUtils.fromJson(req.body(), Query2SQLRequest.class));
     }
 
-    protected Query2SQLResponse convert(Query2SQLRequest q) {
-        if (q == null || CollectionUtils.isEmpty(q.getSelect())) {
+    protected Query2SQLResponse convert(Query2SQLRequest q) throws IOException {
+        if (q == null || StringUtils.isEmpty(q.getText())) {
             throw new IllegalArgumentException("API request structure violated. Please contact the API.");
         }
 
+        QueryTokenizerResponse token = QueryTokenizerSOA.getInstance().tokenizeQuery(q.getText());
+        if (token == null || CollectionUtils.isEmpty(token.getSelect())) {
+            return EMPTY_RESPONSE;
+        }
+
         // Split the list into (a) columns names and (b) table names
-        Set<String> selectItemsForColumns = q.getSelect()
+        Set<String> selectItemsForColumns = token.getSelect()
                 .stream()
                 .filter((e) -> Query2SQLTokenHandler.getInstance().getTableNames(e) != null)
                 .collect(Collectors.toSet());
 
-        Set<String> selectItemsForTable = q.getSelect()
+        Set<String> selectItemsForTable = token.getSelect()
                 .stream()
                 .filter((e) -> Query2SQLTokenHandler.getInstance().getColumnNames(e) != null)
                 .collect(Collectors.toSet());
 
         String aTableName = determineTableNameToSelect(selectItemsForColumns, selectItemsForTable);
         if (StringUtils.isEmpty(aTableName)) {
-            return new Query2SQLResponse("");
+            return EMPTY_RESPONSE;
         }
 
         List<String> selectingColumnNames = determineColumnsNamesToSelect(selectItemsForColumns, aTableName);
 
-        return new Query2SQLResponse("SELECT " + StringUtils.join(selectingColumnNames, ", ") + " FROM tm." + aTableName);
+        return new Query2SQLResponse(
+                selectingColumnNames,
+                "SELECT " + StringUtils.join(selectingColumnNames, ", ") + " FROM tm." + aTableName
+        );
     }
 
     private String determineTableNameToSelect(Set<String> selectItemsForColumns, Set<String> selectItemsForTable) {
--- querytoken-to-sql/src/main/java/com/wsdhaka/gdvf/query2sql/Query2SQLResponse.java
+++ querytoken-to-sql/src/main/java/com/wsdhaka/gdvf/query2sql/Query2SQLResponse.java
@@ -1,10 +1,22 @@
 package com.wsdhaka.gdvf.query2sql;
 
+import java.util.List;
+
 public class Query2SQLResponse {
+    private List<String> columnNames;
     private String sql;
 
-    public Query2SQLResponse(String sql) {
+    public Query2SQLResponse(List<String> columnNames, String sql) {
+        setColumnNames(columnNames);
         setSql(sql);
+    }
+
+    public List<String> getColumnNames() {
+        return columnNames;
+    }
+
+    public void setColumnNames(List<String> columnNames) {
+        this.columnNames = columnNames;
     }
 
     public String getSql() {
