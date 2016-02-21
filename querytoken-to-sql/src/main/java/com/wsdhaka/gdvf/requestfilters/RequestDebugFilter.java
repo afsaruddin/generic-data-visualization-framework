e045eba54ccbb755f11d828756279b1638384041
Afsar Uddin <afsar.uddin@widespace.com>
2016-02-21 15:57:29 +0600

    Some more denbugging

--- querytoken-to-sql/src/main/java/com/wsdhaka/gdvf/requestfilters/RequestDebugFilter.java
+++ querytoken-to-sql/src/main/java/com/wsdhaka/gdvf/requestfilters/RequestDebugFilter.java
@@ -5,10 +5,10 @@ import spark.Spark;
 public final class RequestDebugFilter {
     public final static void apply() {
         Spark.before((request, response) -> {
-            System.out.println("Request: " + request.url() + "?" + request.params());
+            System.out.println("Request: " + request.url() + ". Params: " + request.params());
 
             if ("POST".equalsIgnoreCase(request.requestMethod())) {
-                System.out.println("\n" + request.body());
+                System.out.println(request.body());
             }
         });
     }
