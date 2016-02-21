bfc17a6754f5fb2f6ca9949cdfbea957a7a5ee8f
Shafiul tareq <shafiul.hasan@widespace.com>
2016-02-21 23:41:11 +0600

    Introduce exception handling in the code

--- query-handler/util/dbutil.py
+++ query-handler/util/dbutil.py
@@ -1,29 +1,33 @@
 
 import psycopg2
-import pprint
+
 
 class DbUtil(object):
     @staticmethod
     def execute_query( query):
 
-        conn_string = "host='localhost' dbname='gdvf' user='gdvf_user' password='gdvf_PASS'"
+        try:
+            conn_string = "host='localhost' dbname='gdvf' user='gdvf_user' password='gdvf_PASS'"
 
-        # print the connection string we will use to connect
-        #print "Connecting to database\n	->%s" % (conn_string)
+            # print the connection string we will use to connect
+            #print "Connecting to database\n	->%s" % (conn_string)
 
-        # get a connection, if a connect cannot be made an exception will be raised here
-        conn = psycopg2.connect(conn_string)
+            # get a connection, if a connect cannot be made an exception will be raised here
+            conn = psycopg2.connect(conn_string)
 
-        # conn.cursor will return a cursor object, you can use this cursor to perform queries
-        cursor = conn.cursor()
+            # conn.cursor will return a cursor object, you can use this cursor to perform queries
+            cursor = conn.cursor()
 
-        # execute our Query
-        cursor.execute(query)
+            # execute our Query
+            cursor.execute(query)
 
-        # retrieve the records from the database
+            # retrieve the records from the database
 
-        column_names = [desc[0] for desc in cursor.description]
-        records = cursor.fetchall()
+            column_names = [desc[0] for desc in cursor.description]
+            records = cursor.fetchall()
 
-        return {'column_names': column_names,
-                'data': records }
+            return {'column_names': column_names,
+                    'data': records,
+                    'success': True }
+        except:
+            return {'success': False }
--- query-handler/util/httputil.py
+++ query-handler/util/httputil.py
@@ -5,15 +5,15 @@ import json
 class HttpUtil(object):
     @staticmethod
     def post_url(url, request_body=None):
+        try:
+            post_request = requests.post(url, data=json.dumps(request_body), headers={"content-type": "text/javascript"})
 
-        post_request = requests.post(url, data=json.dumps(request_body), headers={"content-type": "text/javascript"})
+            if post_request.status_code == 200:
 
-        if post_request.status_code == 200:
+                return {'success': True,
+                        'data': post_request.json()}
 
-            return {'success': True,
-                    'data': post_request.json()}
-
-        else:
+            else:
+                return {'success': False}
+        except:
             return {'success': False}
-
-        return {'success': False}
